d3072b6dcb81ed6fa8226e18e119ae846141b106
henrique <henrique.dessen@icloud.com>
2022-07-21 00:46:23 -0300

    Correção

--- alura-stickers/src/main/java/br/com/stickers/App.java
+++ alura-stickers/src/main/java/br/com/stickers/App.java
@@ -1,21 +1,41 @@
 package br.com.stickers;
 
+import br.com.stickers.controllers.CreateImage;
+import br.com.stickers.model.ExtractIMDB;
+import br.com.stickers.model.ExtractNasa;
+import br.com.stickers.model.Image;
 import br.com.stickers.service.ApiConfiguration;
-import br.com.stickers.service.RequestMovie;
+import br.com.stickers.service.GenerateRequest;
+import br.com.stickers.service.Request;
 import br.com.stickers.view.View;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.List;
 
 public class App {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
 
-    RequestMovie request = new RequestMovie();
-    ApiConfiguration api = new ApiConfiguration();
+    Request request = new Request();
+    ExtractIMDB extract = new ExtractIMDB();
+    View view = new View();
+    String text = "HUE";
 
-    try {
-      String body = request.get(api.getUrl() + api.getKey());
-      new View().mostrarJson(body);
-    } catch (Exception ex) {
-      System.out.println(ex.getMessage());
+    String body = request.get(GenerateRequest.imdb.formatedUrl());
+    List<Image> images = extract.extractImages(body);
+
+    var createImage = new CreateImage();
+
+    for(int i = 0; i < 3;  i++){
+
+      Image image = images.get(i);
+      InputStream inputStream = new URL(image.getUrl()).openStream();
+      createImage.builder(inputStream,image.getTitle(), text);
+      view.mostrarJson(images.get(i));
+
     }
+
   }
 }
--- alura-stickers/src/main/java/br/com/stickers/service/ApiConfiguration.java
+++ alura-stickers/src/main/java/br/com/stickers/service/ApiConfiguration.java
@@ -6,21 +6,19 @@ import java.util.Properties;
 
 public class ApiConfiguration {
 
-  private static final String URL = "api.imdb.endpoint-moc";
-  private static final String KEY = "api.imdb.key-moc";
-
   private static Properties getProperties() throws IOException {
     final Properties properties = new Properties();
-    FileInputStream file = new FileInputStream("src/main/resources/application.properties");
+    FileInputStream file = new FileInputStream("alura-stickers/src/main/resources/application.properties");
     properties.load(file);
     return properties;
   }
 
-  public String getUrl() throws IOException {
-    return getProperties().getProperty(URL);
-  }
+  public String getValueOfProperties(String key)  {
+    try{
+      return getProperties().getProperty(key);
+    }catch (IOException ex){
+      throw new RuntimeException(ex);
+    }
 
-  public String getKey() throws IOException {
-    return getProperties().getProperty(KEY);
   }
 }
